eea25f80b107f39add4b769c881def83f05804d9
SAMEER GUPTA <32673052+Sameer079@users.noreply.github.com>
2021-04-15 13:26:45 +0530

    Update createboard.c

--- 3_Implementation/src/createboard.c
+++ 3_Implementation/src/createboard.c
@@ -1,1 +1,19 @@
+#include "header.h"
 
+void create_board()
+{
+	printf("\n\n\tTic Tac Toe\n\n");
+    printf("Player 1 (X) -- Player 2 (O)\n\n");
+    printf("     |     |     \n");
+    printf(" %c   | %c   | %c   \n",box[1],box[2],box[3]);
+    printf("_____|_____|_____\n");
+    printf("     |     |      \n");
+    printf(" %c   | %c   | %c   \n",box[4],box[5],box[6]);
+    printf("_____|_____|_____\n");
+    printf("     |     |      \n");
+    printf(" %c   | %c   | %c   \n",box[7],box[8],box[9]);
+    printf("_____|_____|_____\n");
+    printf("     |     |      \n");
+
+
+}
