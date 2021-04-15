8492a15334e3fc916a77c101ac2b8c009ea46c4b
SAMEER GUPTA <32673052+Sameer079@users.noreply.github.com>
2021-04-15 13:27:35 +0530

    Update markboard.c

--- 3_Implementation/src/markboard.c
+++ 3_Implementation/src/markboard.c
@@ -1,1 +1,27 @@
+#include "header.h"
 
+void mark_board( int choice, char mark)
+{
+    if(choice==1 && box[1]=='1')
+        box[1]=mark;
+    else if(choice==2 && box[2]=='2')
+        box[2]=mark;
+    else if(choice==3 && box[3]=='3')
+        box[3]=mark;
+    else if(choice==4 && box[4]=='4')
+        box[4]=mark;
+    else if (choice==5 && box[5]=='5')
+        box[5]=mark;
+    else if (choice==6 && box[6]=='6')
+        box[6]=mark;
+    else if (choice==7 && box[7]=='7')
+        box[7]=mark;
+    else if (choice==8 && box[8]=='8')
+        box[8]=mark;
+    else if (choice==9 && box[9]=='9')
+        box[9]=mark;
+    else
+    {
+        printf("Invalid move");
+    }
+}
