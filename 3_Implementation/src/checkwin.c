af79c789f35b78fdc9715de54e931769c2874e9a
SAMEER GUPTA <32673052+Sameer079@users.noreply.github.com>
2021-04-15 13:25:30 +0530

    Update checkwin.c

--- 3_Implementation/src/checkwin.c
+++ 3_Implementation/src/checkwin.c
@@ -1,1 +1,32 @@
+#include "header.h"
 
+int check_who_win()
+{
+    if(box[1]==box[2] && box[2]==box[3])
+        return 1;
+    else if(box[4]==box[5] && box[5]==box[6])
+        return 1;                                   // horizontal match
+    else if (box[7]==box[8] && box[8]==box[9])
+        return 1;
+
+    else if (box[1]==box[4] && box[4]==box[7])
+        return 1;
+    else if(box[2]==box[5] && box[5]==box[8])           // vertical match
+        return 1;
+    else if(box[3]==box[6] && box[6]==box[9])
+        return 1;
+
+
+    else if(box[1]==box[5] && box[5]==box[9])
+        return 1;
+    else if(box[3]==box[5] && box[5]==box[7])           //diagonal match
+        return 1;
+
+    else if(box[1]!= '1' && box[2]!= '2' && box[3]!= '3' && box[4]!= '4'&& box[5]!= '5' && box[6]!= '6'&& box[7]!= '7' && box[8]!= '8' && box[9]!='9')  //no match
+        return 0;
+
+    else
+        return -1;
+
+
+}
