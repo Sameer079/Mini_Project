b302c255ce4c6d04923978681503c1f85f789c50
SAMEER GUPTA <32673052+Sameer079@users.noreply.github.com>
2021-04-16 13:52:56 +0530

    Update header.h

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include<stdlib.h>
-static char box[10]={'0','1','2','3','4','5','6','7','8','9'};
+char box[10];
 void create_board();
 void mark_board(int, char);
 int check_who_win();
